877c9fbb9a5406eb74500855b1047a027442ecfd
JonathanSch <jonathansch7@gmail.com>
2020-07-21 19:51:10 -0500

    add clase-repaso

---
+++ clase-repaso/clase-repaso.js
@@ -0,0 +1,20 @@
+var nombre = 'Jonathan';
+
+console.info(nombre);
+
+let apellido = 'Schneider';
+
+console.info(apellido);
+
+const edad = 16;
+
+//Funcion anonima
+ const resta = function(numero1,numero2){
+     return numero1 - numero2;
+ }
+ console.log(resta(134,10))
+const suma = (numero1,numero2) => {
+    return numero1 + numero2;
+}
+
+6 === 5 ? console.info('E verda') : console.info('No he verda');
